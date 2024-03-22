cec25332d3bec822ee679124e05a559545ec9c98
Sohaib-Kamal <syedsohaibkamal@gmail.com>
2024-03-23 00:48:40 +0500

    added a console

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to number guesing game.");
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
