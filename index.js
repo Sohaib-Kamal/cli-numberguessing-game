4164d7a8c88ef19470d3f236c06cfb060a5f18d1
Sohaib-Kamal <syedsohaibkamal@gmail.com>
2024-03-23 00:54:19 +0500

    corrected spelling

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.floor(Math.random() * 6 + 1);
-console.log("Welcome to number guesing game.");
+console.log("Welcome to number guessing game.");
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
